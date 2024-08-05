6546165b7cc503d55a8e61ab73d55fd453408eac
Kai Jing <>
2024-08-06 09:54:43 +1200

    add banner component.

---
+++ frontend/src/components/BannerComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="banner-container">
+    <ChatbotComponent class="clinic-search"></ChatbotComponent>
+    <div class="img-area">
+      <img src="@/assets/docotor.png" alt="docotor img" class="img-docotor" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatbotComponent from "./ChatbotComponent.vue";
+export default {
+  name: "BannerComponent",
+  components: {
+    ChatbotComponent,
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.banner-container {
+  display: flex;
+}
+.clinic-search {
+  flex: 2;
+}
+.img-area {
+  flex: 1;
+}
+.img-docotor {
+  width: 40vw;
+  margin-right: 80px;
+}
+</style>
