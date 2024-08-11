791a035dcb9f04a9ec84c541c813a377e945bdf8
Kai Jing <kai0108jing@gmail.com>
2024-08-12 11:46:45 +1200

    improve code structure

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,13 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueGoogleMaps from "@fawmi/vue-google-maps";
-// import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
-
-// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-//   app.component(key, component);
-// }
 
 app
   .use(router)
