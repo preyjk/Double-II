6d4d0a678641ca2344f7955f02221c8186c2835b
Kai Jing <kai0108jing@gmail.com>
2024-08-13 14:36:13 +1200

    add doctor schedule for admin end

---
+++ frontend/src/components/admin/DoctorSchedule.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>doctor Scheduling</h2>
+    <!-- Scheduling logic and UI goes here -->
+    <el-calendar />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DoctorSchedule",
+};
+</script>
