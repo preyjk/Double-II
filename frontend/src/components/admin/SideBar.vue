ab22076b0894571ac3dc3dd22e832ba8daef237e
Kai Jing <kai0108jing@gmail.com>
2024-08-24 21:50:33 +1200

    add admin side bar structure

---
+++ frontend/src/components/admin/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sidebar"></div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
