403a99321ec4ad095e007b3e09ebdfab3de79662
Jack <18553523055@163.com>
2024-08-27 09:49:59 +1200

    import elementui icon and update dashboard sidebar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,9 +4,13 @@ import router from "./router";
 import VueGoogleMaps from "@fawmi/vue-google-maps";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import store from "@/store";
-const app = createApp(App);
 
+const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app
   .use(router)
   .use(VueGoogleMaps, {
--- frontend/src/views/admin/DashboardPage.vue
+++ frontend/src/views/admin/DashboardPage.vue
@@ -4,54 +4,119 @@
       <el-aside class="dashboard-left">
         <div class="sidebar-header">
           <!-- <img src="logo.png" alt="Logo" class="logo" /> -->
-          <h2 class="title">My Dashboard</h2>
+          <h2 class="title">FRW</h2>
         </div>
 
         <div class="clinic-info">
-          <p>诊所名称: <strong>ABC Clinic</strong></p>
-          <p>地址: <strong>123 Main St</strong></p>
+          <el-icon>
+            <LocationInformation />
+          </el-icon>
+          <p>ABC Clinic</p>
+          <p>123 Main St</p>
         </div>
 
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          :collapse="isCollapse"
-          @open="handleOpen"
-          @close="handleClose"
-        >
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>导航</span>
-            </template>
-            <el-menu-item-group title="组 1">
-              <el-menu-item index="1-1">项目一</el-menu-item>
-              <el-menu-item index="1-2">项目二</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="组 2">
-              <el-menu-item index="1-3">项目三</el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title><span>更多</span></template>
-              <el-menu-item index="1-4-1">项目四</el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
+        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+          <el-radio-button :value="false">expand</el-radio-button>
+          <el-radio-button :value="true">collapse</el-radio-button>
+        </el-radio-group>
 
-          <el-menu-item index="2">
-            <el-icon><icon-menu /></el-icon>
-            <span>其他导航</span>
+        <el-menu default-active="2" class="el-menu-vertical" :collapse="isCollapse" @open="handleOpen"
+          @close="handleClose">
+          <el-menu-item index="1">
+            <el-icon>
+              <Menu />
+            </el-icon>
+            <span>Dashboard</span>
           </el-menu-item>
 
-          <el-menu-item index="3" disabled>
-            <el-icon><document /></el-icon>
-            <span>禁用项</span>
+          <h1>CLINIC</h1>
+          <el-menu-item index="2">
+            <el-icon>
+              <Calendar />
+            </el-icon>
+            <span>Reservations</span>
+          </el-menu-item>
+
+          <el-menu-item index="3">
+            <el-icon>
+              <User />
+            </el-icon>
+            <span>Patients</span>
           </el-menu-item>
 
           <el-menu-item index="4">
-            <el-icon><setting /></el-icon>
-            <span>设置</span>
+            <el-icon>
+              <Odometer />
+            </el-icon>
+            <span>Treatments</span>
+          </el-menu-item>
+
+          <el-menu-item index="5">
+            <el-icon>
+              <Avatar />
+            </el-icon>
+            <span>Staff List</span>
+          </el-menu-item>
+
+          <h1>FINANCE</h1>
+          <el-menu-item index="6">
+            <el-icon>
+              <CreditCard />
+            </el-icon>
+            <span>Accounts</span>
+          </el-menu-item>
+
+          <el-menu-item index="7">
+            <el-icon>
+              <Histogram />
+            </el-icon>
+            <span>Sales</span>
+          </el-menu-item>
+
+          <el-menu-item index="8">
+            <el-icon>
+              <Tickets />
+            </el-icon>
+            <span>Purchases</span>
+          </el-menu-item>
+
+          <el-menu-item index="9">
+            <el-icon>
+              <Wallet />
+            </el-icon>
+            <span>Payment Method</span>
+          </el-menu-item>
+
+          <h1>PHYSICAL ASSET</h1>
+          <el-menu-item index="10">
+            <el-icon>
+              <Goods />
+            </el-icon>
+            <span>Stocks</span>
+          </el-menu-item>
+
+          <el-menu-item index="11">
+            <el-icon>
+              <MostlyCloudy />
+            </el-icon>
+            <span>Pheriperals</span>
+          </el-menu-item>
+
+          <el-menu-item index="12">
+            <el-icon>
+              <TrendCharts />
+            </el-icon>
+            <span>Report</span>
+          </el-menu-item>
+
+          <el-menu-item index="13">
+            <el-icon>
+              <Service />
+            </el-icon>
+            <span>Customer Support</span>
           </el-menu-item>
         </el-menu>
+
       </el-aside>
 
       <el-container class="dashboard-right">
@@ -102,14 +167,8 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from "@element-plus/icons-vue";
 
-const isCollapse = ref(true);
+const isCollapse = ref(false);
 
 const handleOpen = (key: string, keyPath: string[]) => {
   console.log(key, keyPath);
@@ -128,6 +187,7 @@ const handleClose = (key: string, keyPath: string[]) => {
 .dashboard-left {
   background-color: #fafbfd;
   width: auto;
+  min-width: 250px;
   display: flex;
   flex-direction: column;
 }
@@ -190,10 +250,14 @@ const handleClose = (key: string, keyPath: string[]) => {
   background: #e5e9f2;
 }
 
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
+.el-menu-vertical {
+  width: 100%;
+}
+
+.el-menu-vertical:not(.el-menu--collapse) {
   min-height: 400px;
   flex: 1;
   overflow-y: auto;
+  width: 100%;
 }
 </style>
