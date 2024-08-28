0628e8f3a2e6fc12d910410dd9c048ae398e08c4
Kai Jing <kai0108jing@gmail.com>
2024-08-28 14:42:21 +1200

    add: chatbot page

--- frontend/src/views/patients/ChatbotPage.vue
+++ frontend/src/views/patients/ChatbotPage.vue
@@ -1,26 +1,37 @@
 <template>
-  <div class="container_body"></div>
+  <div class="container_body">
+    <el-container>
+      <el-aside class="container_right">
+        <chat-sidebar></chat-sidebar>
+      </el-aside>
+      <el-main class="container_main">
+        <chat-panel></chat-panel>
+      </el-main>
+    </el-container>
+  </div>
 </template>
+
 <script>
 import ChatPanel from "@/components/patients/chatbot/ChatPanel.vue";
+import ChatSidebar from "@/components/patients/chatbot/ChatSidebar.vue";
 
 export default {
   components: {
     ChatPanel,
+    ChatSidebar,
   },
   data() {
     return {};
   },
-  computed: {},
   methods: {},
 };
 </script>
+
 <style scoped>
 .container_body {
-  display: flex;
   width: 100%;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+}
+.container_main {
+  padding: 0;
 }
 </style>
